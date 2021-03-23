46d9ded0ee5ddb980a88e77393d0c0e66ce49cb6
Christopher Boyer <christopherbboyer@gmail.com>
2021-03-23 12:18:36 -0400

    Update 02_run_simulations.R

--- 02_simulations/02_run_simulations.R
+++ 02_simulations/02_run_simulations.R
@@ -2,7 +2,7 @@ results_path <- "07_results/01_saved_results/"
 results_filename <- "simulation_results.rds"
 
 if (RERUN_SIMS) {
-  results <- diagnose_design(design, sims = N_SIMS)
+  results <- diagnose_design(designs, sims = N_SIMS)
   write_rds(results, paste0(results_path, results_filename))
 } else {
   results <- read_rds(paste0(results_path, results_filename))
