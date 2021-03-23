af007f733c4dac28fb90d7fb01af908803677539
Christopher Boyer <christopherbboyer@gmail.com>
2021-03-23 12:19:03 -0400

    Add list of binary variables

--- 01_clean_data/01_define_variable_lists.R
+++ 01_clean_data/01_define_variable_lists.R
@@ -23,3 +23,16 @@ act_variables <- c(
   "sexual_threaten_5mo_freq_i_w",
   "sex_forced_other_5mo_freq_i_w"
 )
+
+act_binary_variables <- c(
+  "physical_slap_5mo_w",
+  "physical_push_5mo_w",
+  "physical_punch_5mo_w",
+  "physical_twist_5mo_w",
+  "physical_kick_5mo_w",
+  "physical_choke_5mo_w",
+  "physical_weapon_5mo_w",
+  "sex_forced_intercourse_5mo_w",
+  "sexual_threaten_5mo_w",
+  "sex_forced_other_acts_5mo_w"
+)
