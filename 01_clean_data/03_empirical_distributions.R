7143d1c845ac8524253b918b068901cfdf3e8280
Christopher Boyer <christopherbboyer@gmail.com>
2022-04-11 23:52:00 -0400

    add nb fit

--- 01_clean_data/03_empirical_distributions.R
+++ 01_clean_data/03_empirical_distributions.R
@@ -7,6 +7,9 @@ data_list <-
     df = subset(elw, Z == 0)
   )
 
+
+# fit zero-inflated poisson -----------------------------------------------
+
 mod <- cmdstan_model("01_clean_data/fit_zip_to_data.stan")
 
 params <- sapply(
@@ -23,6 +26,29 @@ params <- sapply(
 
 lambda <- params[1, ]
 theta <- params[2, ]
+
+
+# fit negative binomial ---------------------------------------------------
+
+nb_mod <- cmdstan_model("01_clean_data/fit_nb_zip_to_data.stan")
+
+nb_params <- sapply(
+  data_list, 
+  function(x) {
+    fit_mle <- nb_mod$optimize(
+      data = x,
+      seed = 123,
+      algorithm = 'bfgs'
+    )
+    
+    fit_mle$mle(c("lambda", "theta", "phi"))
+  }
+)
+
+nb_lambda <- nb_params[1, ]
+nb_theta <- nb_params[2, ]
+nb_phi <- nb_params[3, ]
+
 
 Rho <- cor(elw[elw$Z == 0, act_variables]) + 0.3
 colnames(Rho) <- NULL
